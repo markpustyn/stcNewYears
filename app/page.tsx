05062b52ff50ce383d358ee7f6e5c8a3b7e94f41
markpustyn <108764244+markpustyn@users.noreply.github.com>
2024-12-31 23:15:26 -0800

    Update page.tsx ops

--- app/page.tsx
+++ app/page.tsx
@@ -169,7 +169,7 @@ export default function Home() {
               </CardHeader>
               <CardContent>
                 <p className="text-gray-600">{item.content}</p>
-              <h2 xlassName="text-xl text-gray-400">Click Here to see more</h2>
+              <h2 className="text-xl text-gray-400">Click Here to see more</h2>
                 {isOpen && (
                   <p className="text-gray-600 mt-2">{item.description}</p>
                 )}
